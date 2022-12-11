e990b4e8817837a39f6cbb0f12602b10add7d58e
David Serrano <72123012+Serrano-source@users.noreply.github.com>
2022-12-12 00:08:40 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,7 @@
 
 	<h1>Gestion Musica</h1>
 	
- <!-- Scripts -->
- @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
 @endsection
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
